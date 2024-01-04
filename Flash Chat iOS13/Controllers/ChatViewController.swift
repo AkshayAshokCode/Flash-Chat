b6abaf42a19653fa6c4bc85c8af876c94961cd7b
akshay ashok <akshayashokan1054@gmail.com>
2024-01-04 07:10:12 +0530

    Log out implemented

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
@@ -15,6 +15,8 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
 
     }
     
@@ -22,4 +24,12 @@ class ChatViewController: UIViewController {
     }
     
 
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+          try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
 }
